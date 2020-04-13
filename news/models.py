0ce70c7c29ee99e6bb3082635f65c885ce5ce4bf
Brock Ramsey <brockramz@gmail.com>
2020-04-13 04:40:07 +0000

    Add model, urls, and views.

--- news/models.py
+++ news/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Posts(models.Model):
+    title = models.TextField()
+    date = models.DateField()
+    user = models.TextField(default='anonymous')
+    post = models.TextField()
--- news/views.py
+++ news/views.py
@@ -1,3 +1,43 @@
+import json
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect
 
-# Create your views here.
+from .models import Posts
+
+
+def posts(request):
+    p = Posts.objects.filter(user=request.user.id)
+    print(request.user.id)
+    print(p.values())
+    return render(request, 'posts.html', {'posts': p})
+
+
+def delete(request, id):
+    p = Posts.objects.filter(id=id).delete()
+    return redirect('/')
+    
+
+def read(request, id):
+    if id >= 1:
+        p = Posts.objects.filter(id=id).first()
+        return render(request, 'read.html', {'posts': p})
+
+
+def post(request):
+    if request.method == 'POST':
+        body = json.loads(request.body.decode('utf-8'))
+        if request.user.id is None:
+            userid = 'anonymous'
+        else:
+            userid = request.user.id
+
+        e = Entries.objects.create(
+            title=body['title'],
+            user=userid,
+            entry=body['content'],
+        )
+        e.save()
+        return HttpResponse("success")
+
+    return render(request, 'news.html')
